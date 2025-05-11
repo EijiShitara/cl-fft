0327cfeabc914d3bed91bf843effad97be4f2e86
EijiShitara <c181408m@alm.icu.ac.jp>
2025-05-11 20:26:12 +0900

    bit reversal permutation

--- src/fft.lisp
+++ src/fft.lisp
@@ -10,7 +10,6 @@
                        (setf count (1+ count)))
                      (setf count (1+ count)))))
         (mapcar #'push-if-even
-                    
                 lst)
           (reverse result)))))
 
@@ -26,7 +25,6 @@
                        (setf count (1+ count)))
                      (setf count (1+ count)))))
         (mapcar #'push-if-odd
-                    
                 lst)
           (reverse result)))))
 
@@ -47,7 +45,7 @@
              (subarray-odd (odd-positions a))
              (y_0 (fft subarray-even))
              (y_1 (fft subarray-odd)))
-        (let ((result (loop for i from 0 to len collect 0)))
+        (let ((result (loop for i from 0 to (- len 1) collect 0)))
           (loop for i from 0 to (- (/ len 2) 1)
                 do (setf (nth i result)
                          (+ (nth i y_0)
@@ -57,11 +55,6 @@
                             (* omega (nth i y_1))))
                    (setf omega (* omega omega-n)))
           result)))))
-
-(mapcar #'abs (fft (loop for i from 1 to 4096 collect (random 1.0))))
-(fft '(1 1  0 1 -1 1 0 1 1 1  0 1 -1 1 0 1 1 1  0 1 -1 1 0 1 1 1  0 1 -1 1 0 1))
-(fft '(1 1 1 1))
-(fft '(1 .5 1 .5))
 
 (defun inverse-fft (a)
   "The length of array a must be a power of 2"
@@ -87,3 +80,41 @@
                             (* omega (nth i y_1))))
                    (setf omega (* omega omega-n)))
           result)))))
+
+;;; Bit-reversal-permutation
+(defun integer->bit-vector (integer &optional dim)
+  "Create a bit-vector from a nonnegative integer."
+  (labels ((integer->bit-list (int &optional accum)
+             (cond ((> int 0)
+                    (multiple-value-bind (i r) (truncate int 2)
+                      (integer->bit-list i (push r accum))))
+                   ((null accum)
+                    (integer->bit-list int (list 0)))
+                   (dim
+                    (let ((len (length accum))) ; need refactoring
+                      (when (>= dim len)
+                        (dotimes (i (- dim len))
+                          (push 0 accum))
+                        accum)))
+                   (t accum))))
+    (coerce (integer->bit-list integer) 'bit-vector)))
+
+(defun bit-vector->integer (bit-vector)
+  "Create a nonnegative integer from a bit-vector."
+  (reduce #'(lambda (first-bit second-bit)
+              (+ (* first-bit 2) second-bit))
+          bit-vector))
+
+(defun f (n &optional dim)
+  (bit-vector->integer
+   (nreverse
+    (integer->bit-vector n dim))))
+
+(defun bit-reversal-permutation (sequence &optional (n (length sequence)))
+  (let ((v (make-array n))
+        (p (round (log n 2))))
+    (loop for val in sequence
+          for m from 0
+          do (setf (aref v (f m p)) val))
+    v))
+    
